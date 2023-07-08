6c549b1929446e41eef0094ce044010400518d7a
Mackowi <dzierzenga.maciej@gmail.com>
2023-07-08 13:16:47 +0200

    login, register, authentication, spinner

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import ForgotPassword from './pages/ForgotPassword'
 import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
@@ -14,13 +17,17 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<SignIn />} />
+          {/* PRIVATE ROUTE -> NESTED ROUTE WITHIT ROUTE WITH PRIVATEROUTE COMPONENT AS ELEMENT */}
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
+      <ToastContainer />
     </>
   )
 }
